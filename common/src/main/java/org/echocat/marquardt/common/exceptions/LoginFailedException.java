6a9d94c289086a6ac1a99cc36f71d0eda14f2f5d
christian.rijke <christian.rijke@teufel.de>
2015-09-02 12:38:42 +0200

    RAS-46 Add login exceptions.

---
+++ common/src/main/java/org/echocat/marquardt/common/exceptions/LoginFailedException.java
@@ -0,0 +1,20 @@
+/*
+ * echocat Marquardt Java SDK, Copyright (c) 2015 echocat
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.echocat.marquardt.common.exceptions;
+
+public class LoginFailedException extends RuntimeException {
+
+    public LoginFailedException() {
+        super();
+    }
+
+    public LoginFailedException(String message) {
+        super(message);
+    }
+}
