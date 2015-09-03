cf21730070bb2b8ded76a863cb33378fec56c3fc
sebastian.schmidt <sebastian.schmidt@teufel.de>
2015-09-03 13:41:51 +0200

    * RAS-38: Moved exception to common module as it's a common exception shared between modules

--- common/src/main/java/org/echocat/marquardt/common/ContentValidator.java
+++ common/src/main/java/org/echocat/marquardt/common/ContentValidator.java
@@ -8,6 +8,7 @@
 
 package org.echocat.marquardt.common;
 
+import org.apache.commons.io.IOUtils;
 import org.echocat.marquardt.common.domain.Certificate;
 import org.echocat.marquardt.common.domain.CertificateFactory;
 import org.echocat.marquardt.common.domain.DeserializingFactory;
@@ -34,10 +35,29 @@ public class ContentValidator {
             }
             throw new InvalidSignatureException();
         } finally {
-            try {
-                inputStream.close();
-            } catch (final IOException ignored) {}
+            IOUtils.closeQuietly(inputStream);
         }
+    }
+
+    private <T extends Signable> Certificate<T> validateAndDeserialize(final byte[] content, final DeserializingFactory<Certificate<T>> signableFactory, final PublicKeyProvider<T> publicKeyProvider) throws IOException {
+        final ByteArrayInputStream inputStream = new ByteArrayInputStream(content);
+        try {
+            final Certificate<T> signable = signableFactory.consume(inputStream);
+
+            final PublicKey publicKey = publicKeyProvider.provide(signable);
+            final int signatureLength = InputStreamUtils.readInt(inputStream);
+            final Signature signature = new Signature(InputStreamUtils.readBytes(inputStream, signatureLength));
+            if (signature.isValidFor(signable.getContent(), publicKey)) {
+                return signable;
+            }
+            throw new InvalidSignatureException();
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+    }
+
+    public <T extends Signable> Certificate<T> deserializeCertificateAndValidateSignature(final byte[] content, final DeserializingFactory<T> wrappedSignableFactory, final PublicKeyProvider<T> provider) throws IOException {
+        return validateAndDeserialize(content, getCertificateFactory(wrappedSignableFactory), provider);
     }
 
     public <T extends Signable> Certificate<T> validateAndDeserializeCertificate(final byte[] content, final DeserializingFactory<T> wrappedSignableFactory, final PublicKey publicKey) throws IOException {
@@ -45,11 +65,11 @@ public class ContentValidator {
     }
 
     public <T extends Signable> T deserialize(final byte[] content, final DeserializingFactory<T> signableFactory) throws IOException {
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(content);
+        final ByteArrayInputStream inputStream = new ByteArrayInputStream(content);
         try {
             return signableFactory.consume(inputStream);
         } finally {
-            inputStream.close();
+            IOUtils.closeQuietly(inputStream);
         }
     }
 
@@ -66,4 +86,8 @@ public class ContentValidator {
         };
     }
 
+    public static interface PublicKeyProvider<T extends Signable> {
+
+        public PublicKey provide(final Certificate<T> signable);
+    }
 }
